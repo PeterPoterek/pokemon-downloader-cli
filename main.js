03827d487d46bc04c9c58c97f7d0af5176ecefe0
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-08 23:50:54 +0100

    checkbox options work in progress

--- main.js
+++ main.js
@@ -3,16 +3,48 @@ import inquirer from "inquirer";
 
 let pokemonName = "";
 
-const questions = {
-  type: "input",
-  name: "pokemonName",
-  message: "Write an pokemon name",
-};
 const getPokemonName = () => {
+  const questions = {
+    type: "input",
+    name: "pokemonName",
+    message: "Write an pokemon name",
+  };
+
   inquirer.prompt(questions).then((anwser) => {
     pokemonName = anwser.pokemonName;
     downloadPokemon();
+    getUserInput();
   });
+};
+
+const getUserInput = () => {
+  const questions = {
+    type: "checkbox",
+    message: "Select:",
+    name: "userInput",
+    choices: [
+      new inquirer.Separator("Which to download "),
+      {
+        name: "Artwork",
+      },
+      {
+        name: "Sprites",
+      },
+      {
+        name: "Stats",
+      },
+    ],
+
+    validate(answer) {
+      if (answer.length < 1) {
+        return "You must chose at least 1 option";
+      }
+
+      return true;
+    },
+  };
+
+  inquirer.prompt(questions).then((answer) => console.log(answer));
 };
 const downloadPokemon = async () => {
   try {
@@ -21,7 +53,7 @@ const downloadPokemon = async () => {
     const response = await data;
     let stats = "";
     for (const stat of response.stats) {
-      stats += `${stat.stat.name} : ${stat.base_stat}\n`;
+      stats += `${stat.stat.name}: ${stat.base_stat}\n`;
     }
 
     const spriteUrl = response.sprites.front_default;
