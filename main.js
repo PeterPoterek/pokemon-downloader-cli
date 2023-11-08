114eba893b73afeec9bdd255ef39f33457d3cd4c
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-08 19:39:55 +0100

    download sprite function

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-let pokemonName = "pikachu";
+import fs from "fs/promises";
 
-const data = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+let pokemonName = "slowpoke";
+
+const download = async () => {
+  try {
+    const data = (await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)).json();
+
+    const response = await data;
+
+    console.log(response);
+
+    const spriteUrl = response.sprites.front_default;
+    const sprite = await fetch(spriteUrl);
+
+    fs.writeFile(`./pokemons/${pokemonName}.png`, sprite.body);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+download();
