37ced61c4232a3fc2976fcbe1128b6e839be96ce
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-08 20:15:59 +0100

    image + stats file now working

--- main.js
+++ main.js
@@ -7,15 +7,21 @@ const download = async () => {
     const data = (await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)).json();
 
     const response = await data;
+    let stats = "";
+    // console.log(response.stats);
+    for (const stat of response.stats) {
+      //   console.log(`${stat.stat.name} : ${stat.base_stat}`);
 
-    console.log(response);
+      stats += `${stat.stat.name} : ${stat.base_stat}\n`;
+    }
 
     const spriteUrl = response.sprites.front_default;
     const sprite = await fetch(spriteUrl);
 
     fs.writeFile(`./pokemons/${pokemonName}.png`, sprite.body);
+    fs.writeFile(`./pokemons/${pokemonName}.txt`, stats);
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 };
 
