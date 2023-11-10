ca003bcfe7f3a53964579f062649980ea82566a2
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-10 02:13:24 +0100

    downloadPokemonData wip

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import inquirer from "inquirer";
+import path from "path";
+import fs from "fs/promises";
 
 const getPokemonName = async () => {
   const questions = {
@@ -11,8 +13,12 @@ const getPokemonName = async () => {
 };
 
 const fetchPokemon = async (pokemonName) => {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-  return await response.json();
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    return await response.json();
+  } catch {
+    console.error("Enter valid Pokemon name");
+  }
 };
 
 const getUserInput = async () => {
@@ -47,12 +53,31 @@ const getAnotherPokeon = async () => {
   return await inquirer.prompt(options);
 };
 
+const createFolder = async (pokemonName) => {
+  fs.mkdir(`./pokedex/${pokemonName}`);
+};
+
+const downloadPokemonData = async (userInput, json, pokemonName) => {
+  createFolder(pokemonName);
+
+  if (userInput.includes("Artwork")) {
+    console.log("Artwork");
+  }
+  if (userInput.includes("Sprites")) {
+    console.log("Sprites");
+  }
+  if (userInput.includes("Stats")) {
+    console.log("Stats");
+  }
+};
+
 const handleLoop = async () => {
   while (true) {
     const pokemonName = await getPokemonName();
     const json = await fetchPokemon(pokemonName.name);
 
     const userInput = await getUserInput();
+    downloadPokemonData(userInput.input, json, pokemonName.name);
 
     const shouldContinue = await getAnotherPokeon();
     if (shouldContinue.continue === "No") break;
